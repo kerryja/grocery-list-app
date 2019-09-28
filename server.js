4e5ea4269f1c3866605242980e230c14c37d56cf
Kerry Jackson <kerry.jackson108@gmail.com>
2019-09-28 16:12:54 -0400

    Implemented Create and Delete functionality

--- server.js
+++ server.js
@@ -1,27 +1,104 @@
 const express = require("express");
 const http = require("http");
-const next = require("next");
-
 const socketIO = require("socket.io");
-
-const port = process.env.PORT || 3000;
-
+const next = require("next");
+// const mongo = require("mongodb");
+// const mongoose = require("mongoose");
+// const db = mongoose.connection;
 const app = express();
+const server = http.Server(app);
+
+const port = normalizePort(process.env.PORT || 3000);
+app.set("port", port);
+const io = socketIO(server);
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
+
+//fake db
+global.gItems = [];
 
 // app.get("/", (req, res) => res.send("Hello"));
 
-const server = http.Server(app);
+//Connect to mongo
+// async function connect() {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1/grocery-app", {
+//       useNewUrlParser: true
+//     });
+//   } catch (error) {
+//     handleError(error);
+//     console.log(error);
+//   }
+// }
 
-server.listen(port, () => console.log(`server is listening on ${port}`));
+// db.once("open", () => {
+//   console.log("MongoDB connected");
+// });
 
-app.use(express.static("public"));
-
-const io = socketIO(server);
+//connect to socket.io
+// io.on("connection", socket => {
+//   console.log("New client connected");
+//   let listItems = db.collection("list-items");
 
 io.on("connection", socket => {
-  console.log("New client connected");
+  socket.on("item", data => {
+    global.gItems.push(data);
+    socket.broadcast.emit("item", data);
+  });
 
-  socket.on("list-item", data => {
-    io.sockets.emit("list-item", data);
+  socket.on("delete", itemID => {
+    console.log(itemID);
+    global.gItems = global.gItems.filter(item => item.id !== itemID);
+    socket.broadcast.emit("delete", itemID);
+  });
+  // setTimeout(function() {
+  //   // To add to the "database"
+  //   //global.gItems.push("From socket.io");
+  //   socket.emit("item", "From socket.io");
+  // }, 3000);
+});
+
+nextApp.prepare().then(() => {
+  // app.get("/items", (req, res) => {
+  //   res.json(items);
+  // });
+
+  app.get("*", (req, res) => {
+    return nextHandler(req, res);
+  });
+
+  //send status
+  // sendStatus = s => {
+  //   socket.emit("status", s);
+  // };
+
+  //get list items from mongo collection
+  // socket.on("list-item", data => {
+  //   let listItems = db.collection("list-items");
+  //   io.sockets.emit("list-item", data);
+  // });
+
+  // listItems.find()
+  // });
+  // });
+
+  // app.use(express.static("public"));
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`Ready on ${port}`);
   });
 });
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
